1a393519aacc0a9be44b9234d98ff1cc47ac3317
fvla <frederick.v.l@gmail.com>
2015-05-27 12:00:22 -0400

    Edited quadratic solver

--- quadr3p/main.pl
+++ quadr3p/main.pl
@@ -1,4 +1,24 @@
-#
-# Hello World Program in Perl
-#
-print "Hello World!\n";
+#!/usr/bin/env perl
+sub f {
+	my ($x, $func) = @_;
+	return eval $func;
+}
+
+my @points;
+
+sub getpoints {
+	my $n = 0;
+	while ($n < shift) {
+		$_ = <STDIN>;
+		unless (/^\d+,\d+$/) {
+			print 'Please enter your points in the format #,#';
+			next;
+		}
+		my ($x, $y) = split /,/;
+		my $point = {
+			x => $x,
+			y => $y
+		}
+		$points[$n++] = $point;
+	}
+}
